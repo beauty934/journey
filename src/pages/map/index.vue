ed73704aab66c2fff826d3db7bc5f6157e14a95c
beauty <2656266096@qq.com>
2019-12-03 18:24:12 +0800

    1203.02

--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
+  <div style="width: 100%; height: 100%;">
+    <map
+      id="map"
+      scale="14"
+      :markers="markers"
+      show-location
+      style="width: 100%; height: 100%;"
+    ></map>
   </div>
 </template>
 
@@ -8,7 +15,13 @@
 export default {
   data () {
     return {
-     
+     markers: [{
+        id: 0,
+        latitude: 23.099994,
+        longitude: 113.324520,
+        width: 50,
+        height: 50
+      }]
       }
   },
   methods: {
@@ -19,6 +32,8 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style >
+body, html {
+   height:100%
+}
 </style>
