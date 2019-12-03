e72fffc5a139c90f84dbcfcb9c541dae58740379
beauty <2656266096@qq.com>
2019-12-03 17:36:51 +0800

    1203.01

--- src/pages/list/index.vue
+++ src/pages/list/index.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
+    <i-card v-for="item in list" :key="item" i-class="split"  :extra="item.name" :thumb="item.image">
+         <view slot="content">{{item.remark}}</view>
+        <view slot="footer">{{item.address}}</view>
+      </i-card>
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
-     
+     list:[]
       }
+  },
+   onLoad (option){
+    this.list = require('@/data/' + option.type + '.json')
+    console.log(this.list)
   },
   methods: {
     },
@@ -18,5 +26,7 @@ export default {
 </script>
 
 <style scoped>
-
+div >>> .split {
+margin-bottom: 10pt;
+}
 </style>
